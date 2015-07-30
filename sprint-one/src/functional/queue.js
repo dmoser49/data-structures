4774ecd405c24581aa75d3287d83d74abdc81be6
MKS-students <mks.students@hackreactor.com>
2015-07-30 11:20:44 -0500

    Nearly complete.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,37 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var firstElement = 0;
+  //var itemsRemoved =0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    if (size > 0){
+      size--;
+      var element = storage[firstElement];
+      delete storage[firstElement];
+      if (size > 0) {
+        firstElement++;
+      }
+    console.log(element)      //console.log(firstElement)
+      return element;
+
+    }
+
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
 };
+
