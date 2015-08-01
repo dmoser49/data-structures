b86b52223809707210583ef315b5f035ac6ce55f
MKS-students <mks.students@hackreactor.com>
2015-07-31 20:00:36 -0500

    finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,16 +1,47 @@
 var LinkedList = function(){
-  var list = {};
+  var list={}
   list.head = null;
   list.tail = null;
+  list.nextToLast= null;
 
-  list.addToTail = function(value){
+  list.addToTail = function(element){
+    var newNode = Node(element)
+    if (list.head === null){
+      list.head = newNode;
+      list.tail = newNode;
+      list.nextToLast = newNode;
+    }
+    list.nextToLast = list.tail;
+    list.tail = newNode;
+    list.nextToLast.next = list.tail.value;
   };
 
   list.removeHead = function(){
+    var formerHead = list.head.value;
+    var second = list.head.next;
+    list.head = Node(second);
+
+    return formerHead;
   };
 
   list.contains = function(target){
-  };
+
+    /////////////////////
+    var checkValue = function (node){
+      if (node.value===target){
+        return true
+      }
+      else if(node.next===null){
+        return false
+      }
+      else {
+
+        return checkValue(Node(node.next))
+      }
+
+    }
+      return checkValue(list.head)
+  }
 
   return list;
 };
