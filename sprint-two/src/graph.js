c3396e6d902d7fea4723e4bf7ce652c9d746a097
MKS-students <mks.students@hackreactor.com>
2015-08-01 16:42:52 -0500

    Finished tree, started graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,24 +1,57 @@
 
 
 var Graph = function(){
+
+  this.nodes = {};
+  this.edges = [];
+
 };
 
 Graph.prototype.addNode = function(node){
+  this.nodes[node] = node;
 };
 
 Graph.prototype.contains = function(node){
+  if (this.nodes[node]){
+    return true
+  }
+  return false
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.nodes[node]
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  var doesHaveEdge = false;
+  // for(var i= 0;i<this.edges.length;i++)
+
+  _.each(this.edges, function(edge){
+
+    // console.log('fromNode',fromNode,'toNode', toNode)
+    // console.log('array',[fromNode, toNode])
+  if (edge[0] === fromNode && edge[1] === toNode){
+    doesHaveEdge = true
+  }
+
+})
+  return doesHaveEdge;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.edges.push([fromNode, toNode])
+  this.edges.push([toNode, fromNode])
+  console.log(this.edges)
 };
-
 Graph.prototype.removeEdge = function(fromNode, toNode){
+   _.each(this.edges, function(edge, i, collection){
+     if(edge[0]===fromNode && edge[1] === toNode){
+       collection.splice(i, 1)
+     }
+     if(edge[0]===toNode && edge[1] === fromNode){
+       collection.splice(i, 1)
+     }
+   })
 };
 
 Graph.prototype.forEachNode = function(cb){
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,10 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
+  _.extend(newTree, treeMethods)
+
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,12 +17,25 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value))
 };
 
-treeMethods.contains = function(target){
+treeMethods.contains = function (target){
+    var isFound = false
+    var checkValue = function (node){
 
-};
+      for (var i = 0;i<node.children.length;i++){
+        if(node.children[i].value===target){
+          isFound = true
+        }
+        else if(node.children[i].children){
+          checkValue(node.children[i])
+        }
+      }
+    }
+    checkValue(this);
+    return isFound;
+  }
 
 
 /*
