24af96e5830b40fc0c0962dee318ea95290420bf
MKS-students <mks.students@hackreactor.com>
2015-08-03 09:01:12 -0500

    Finish set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,34 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage.push(item)
+  console.log('this._storage',this._storage)
+  console.log('item', item)
 };
 
 setPrototype.contains = function(item){
+  var found = false
+  _.each(this._storage, function(element){
+    if(element===item){
+      found = true
+    }
+
+  })
+  return found
 };
 
 setPrototype.remove = function(item){
+  _.each(this._storage, function(element, i, collection){
+    if (element === item){
+      collection.splice(i, 1)
+    }
+  })
 };
 
 /*
